e0dde30ef246aaf2e9a7585084ee7de91d7b4f7f
Charles Ai <kailun.ai27@gmail.com>
2018-05-30 09:27:14 -0400

    May 29, 2018

--- js/java.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("h3").mouseover(function() {
-		/* Act on the event */
-		$(this).css("font-size","1.5em");
-	});
-	$("h3").mouseout(function() {
-		$(this).css("font-size","2.5em");
-	});
-});
